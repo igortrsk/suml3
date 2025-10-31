ee2483bd50d09e0ebff2f0578b266d545dc5b0ad
igortrsk <56490941+igortrsk@users.noreply.github.com>
2025-10-31 02:03:18 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,53 +3,26 @@ import pandas as pd
 import time
 import matplotlib as plt
 import os
-# zaczynamy od zaimportowania bibliotek
 st.set_page_config(page_title="Great translation website")
 st.success('Gratulacje! Z powodzeniem uruchomiłeś aplikację')
-# streamlit jest wykorzystywany do tworzenia aplikacji
-# z tego powodu dobrą praktyką jest informowanie użytkownika o postępie, błędach, etc.
 
-# Inne przykłady do wypróbowania:
-# st.balloons() # animowane balony ;)
-# st.error('Błąd!') # wyświetla informację o błędzie
-# st.warning('Ostrzeżenie, działa, ale chyba tak sobie...')
-# st.info('Informacja...')
-# st.success('Udało się!')
-
-# st.spinner()
-# with st.spinner(text='Pracuję...'):
-    # time.sleep(2)
-    # st.success('Done')
-# możemy dzięki temu "ukryć" późniejsze ładowanie aplikacji
-
-st.title('Lab05. Streamlit')
-# title, jak sama nazwa wskazuje, używamy do wyświetlenia tytułu naszej aplikacji
+st.title('Streamlit')
 
 st.header('Wprowadzenie do zajęć')
-# header to jeden z podtytułów wykorzystywnaych w Streamlit
 
 st.subheader('O Streamlit')
-# subheader to jeden z podtytułów wykorzystywnaych w Streamlit
 
 st.text('To przykładowa aplikacja z wykorzystaniem Streamlit')
-# text używamy do wyświetlenia dowolnego tekstu. Można korzystać z polskich znaków.
 
 st.write('Streamlit jest biblioteką pozwalającą na uruchomienie modeli uczenia maszynowego.')
-# write używamy również do wyświetlenia tekstu, różnica polega na formatowaniu.
 
 st.code("st.write()", language='python')
-# code może nam się czasami przydać, jeżeli chcielibyśmy pokazać np. klientowi fragment kodu, który wykorzystujemy w aplikacji
 
 with st.echo():
     st.write("Echo")
-# możemy też to zrobić prościej używając echo - pokazujemy kod i równocześnie go wykonujemy
 
 df = pd.read_csv("DSP_4.csv", sep = ';')
 st.dataframe(df)
-# musimy tylko pamiętać o właściwym określeniu separatora (w tym wypadku to średnik)
-# masz problem z otworzeniem pliku? sprawdź w jakim katalogu pracujesz i dodaj tam plik (albo co bardziej korzystne - zmień katalog pracy)
-# os.getcwd() # pokaż bieżący katalog
-# os.chdir("") # zmiana katalogu
 
 st.header('Przetwarzanie języka naturalnego')
 
